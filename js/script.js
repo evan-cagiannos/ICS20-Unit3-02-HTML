ece1ff62a4c659b691b95e5b6d4e51291d663eae
evan-cagiannos <99355231+evan-cagiannos@users.noreply.github.com>
2022-04-05 17:21:49 +0000

    Everything

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Created by Evan Cagiannos
+// ICS2O-UNIT2-02-HTML
+// Created on: 2022-March-10 
+//This file contains the JS functions for index.html
+
+/**
+* Does basic math
+*/
+function doMathClicked() {
+  // this function does basic math
+  // input
+  const length = parseInt(document.getElementById('length-of-pyramid').value)
+  const width = parseInt(document.getElementById('width-of-pyramid').value)
+  const height = parseInt(document.getElementById('height-of-pyramid').value)
+
+  // process
+  const volume = (length * width * height) / 3
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is: ' + volume + ' cm3'
+}
